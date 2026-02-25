e840db17a6340b7fbd99ade5e2826ed5abac842d
sanbestia <sebayanic@gmail.com>
2026-02-25 21:48:03 +0000

    added clock and dt

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
+    # Initialize pygame module
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+    
+    # Initialize clock and delta time control
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Main inf loop
     while True:
+        # Log game state data to game_state.jsonl
         log_state()
+        
         for event in pygame.event.get():
+            # Allows exiting game on window closing
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        
+        # Wait until 1/60 of a second has passed and register time since last call
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
