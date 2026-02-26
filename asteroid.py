5c7c0c7818816aacf8a7a977f48f998ded657cf0
sanbestia <sebayanic@gmail.com>
2026-02-26 00:14:35 +0000

    added asteroids splitting when shot

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_SPLIT_SPEED_INCREASE    
 
 
 class Asteroid(CircleShape):
@@ -15,6 +17,27 @@ class Asteroid(CircleShape):
         
     def update(self, dt):
         self.position += self.velocity * dt
+        
+    
+    # Call when asteroid is hit with a shot
+    def split(self):
+        # Always delete current asteroid
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            # If asteroid is big, it splits into fragments, two new asteroids are created with some extra speed
+            split_angle = random.uniform(20, 50)
+            first_new_velocity = self.velocity.rotate(split_angle) * ASTEROID_SPLIT_SPEED_INCREASE
+            second_new_velocity = self.velocity.rotate(-split_angle) * ASTEROID_SPLIT_SPEED_INCREASE
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            first_new_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
+            first_new_asteroid.velocity = first_new_velocity
+            second_new_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
+            second_new_asteroid.velocity = second_new_velocity
+            
+        
         
         
 def main():
--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def main():
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
                 
         
         # Refresh screen
